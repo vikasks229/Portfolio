7d21d570335005ad73fd2353122368dfd2507c9f
vikasks229 <vikasks229@gmail.com>
2025-07-27 13:39:43 +0530

    Added 3D animations

--- hide.js
+++ hide.js
@@ -1,1 +1,17 @@
 document.addEventListener("contextmenu", (e) => e.preventDefault());
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show-on-scroll');
+      observer.unobserve(entry.target); // Only animate once
+    }
+  });
+});
+
+const animatedSections = document.querySelectorAll(
+  '#project1, #project2, #project3, #rock-paper-game-project4, #currency-converter-project-5, #TicTacToe-project-6, #skills, #education'
+);
+
+animatedSections.forEach(section => {
+  observer.observe(section);
+});
